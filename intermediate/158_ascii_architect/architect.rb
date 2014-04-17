a0e636482027b1e60ef8a45b3f0e125cc44534d3
a-out <richbone227@gmail.com>
2014-04-17 00:02:10 -0400

    parsing and etc finished, need to work on translate()

--- intermediate/158_ascii_architect/architect.rb
+++ intermediate/158_ascii_architect/architect.rb
@@ -1,8 +1,9 @@
-module AsciiArchitect   
+require 'pp'
+module AsciiArchitect
 
    # converts a letter a-j to the proper line
    def line(letter)
-      if (letter.size != 1) || (!letter.is_a? String) || 
+      if (letter.size != 1) || (!letter.is_a? String) ||
          (!'abcdefghij'.include? letter)
          raise ArgumentError, "not a letter a-j", caller
       end
@@ -11,22 +12,50 @@ module AsciiArchitect
       '++--***...'.split('').slice(0, letter_value).join
    end
 
+   def parse(input_str)
+      split = input_str.scan(/[1-9]*[a-j]/)
+      parsed = []
+
+      split.map { |token|
+         if token.size > 1
+            { num: token[0].to_i, letter: token[1] }
+         else
+            { letter: token[0], num: nil }
+         end
+      }
+   end
+
+   def process_line(token)
+      if !token[:num].nil?
+         " " * token[:num] + line(token[:letter])
+      else
+         line(token[:letter])
+      end
+   end
+
    def translate(lines)
       translated = []
+
+      # skim off a character from each line,
+      # essentially flipping the shape by 90 degrees
       for i in 0..lines.size do
-         new_string = "" 
-         lines.map { |line|            
+         new_string = ""
+         lines.map { |line|
             if line.size > i
-               
+               new_string << line[i]
             end
          }
 
-         translated << new_string
+         translated << new_string unless new_string.size == 0
       end
 
-      return translated.reverse
+      # pad the left side of the lines with zeroes,
+      # essentially pushing the shape to the right side
+      translated.reverse.map { |line|
+         line.rjust(translated.size)
+      }
    end
-  
+
 end
 
 include AsciiArchitect
@@ -37,9 +66,13 @@ def main
       strings << line(char)
    }
 
-   puts translate(strings)
+   # pp translate(strings)
+   parsed = parse("j3f3e")
+   pp parsed
 
-
+   puts translate(parsed.map { |token|
+      process_line(token)
+   })
 end
 
 if $0 == __FILE__
