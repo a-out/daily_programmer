a6e63a1dae3fd8092bc498d2d588c9217840630c
a-out <richbone227@gmail.com>
2014-05-01 14:59:15 -0400

    calculate letter distance of key

--- easy/88_vigenere_cipher/cipher.rb
+++ easy/88_vigenere_cipher/cipher.rb
@@ -6,19 +6,20 @@ def decrypt(ciphertext, key)
 end
 
 def encrypt(plaintext, key)
-   alphabet = (a..z)
+   alphabet = ('a'..'z').to_a
    # stretch key out to be size of plaintext
    key_stretched = (key * (plaintext.length / key.length + 1)
                   ).split('').first(plaintext.length)
-   key_stretched.map { |char|
-       
-   }.join
+   
+   letter_distances = key_stretched.map { |char|
+      char.downcase.ord - 96 
+   }
 end
 
 def main
    plaintext = 'THECAKEISALIE'
    key = 'GLADOS'
-   encrypt(plaintext, key)
+   pp encrypt(plaintext, key)
 end
 
 if $0 == __FILE__
