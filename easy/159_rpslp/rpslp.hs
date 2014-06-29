1b120fdc1aac843d064f7e5b8bd18cacdd4ed606
a-out <richbone227@gmail.com>
2014-06-29 16:26:15 -0400

    implemented beats

--- easy/159_rpslp/rpslp.hs
+++ easy/159_rpslp/rpslp.hs
@@ -1,5 +1,13 @@
+import qualified Data.Map as Map
+import System.Random
 
-data Gesture = Rock | Paper | Scissors | Lizard | Spock deriving (Show, Read, Enum)
+data Gesture = Rock | Paper | Scissors | Lizard | Spock deriving (Show, Read, Eq, Ord)
 
-combatMap :: EnumMap Gesture Gesture
-combatMap = fromList [(fromEnum Paper, fromEnum Rock)]
+beats :: Gesture -> Gesture -> Maybe Bool
+beats x y = Map.lookup x beatsMap >>= \l -> return (y `elem` l)
+   where beatsMap = Map.fromList [(Rock, [Scissors, Lizard]),
+                                  (Paper, [Rock, Spock]),
+                                  (Scissors, [Paper, Lizard]),
+                                  (Lizard, [Spock, Paper]),
+                                  (Spock, [Rock, Scissors])
+                                 ]
